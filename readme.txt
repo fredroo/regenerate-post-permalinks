1b7d0be02608243014bb735f1198a51b079785a3
almos20 <almos20@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-21 07:49:21 +0000

    support wordpress 3.8
    git-svn-id: https://plugins.svn.wordpress.org/regenerate-post-permalinks/trunk@826486 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ License: GPLv2 or later
 Donate link: http://sandorkovacs84.wordpress.com/
 Tags: post, permalinks
 Requires at least: 3.1.0
-Tested up to: 3.6.0
-Stable tag: 1.0.0
+Tested up to: 3.8.0
+Stable tag: 1.0.1
 
 This plugin can help you to regenerate all your permalinks based on the post titles. 
 
--- regenerate-post-permalink.php
+++ regenerate-post-permalink.php
@@ -4,7 +4,7 @@
   Plugin URI: http://www.sandorkovacs.ro/blog/simple-post-meta-manager-wordpress-plugin/
   Description: This plugin can help you to regenerate all your permalinks based on the post titles.
   Author: Sandor Kovacs
-  Version: 1.0.0
+  Version: 1.0.1
   Author URI: http://sandorkovacs.ro/en/
  */
 
